5108fdad4444f0a5f8280476208121b08b39f9a0
YosefSchwartz <73064850+YosefSchwartz@users.noreply.github.com>
2021-01-14 10:58:52 +0200

    final touch-ups

--- src/GraphAlgo.py
+++ src/GraphAlgo.py
@@ -290,9 +290,6 @@ class GraphAlgo(GraphAlgoInterface):
 
         # Insert the x and y value to lists, and if None, compute random value between [min,max]
         X, Y = [], []
-        # fig = plt.gcf()
-        # size = fig.get_size_inches() * fig.dpi
-
         for n in self.get_graph().get_all_v().values():
             if n.getPos() is None:
                 n.setPos((rnd.uniform(minX, maxX), rnd.uniform(minY, maxY), rnd.uniform(0, 10)))
